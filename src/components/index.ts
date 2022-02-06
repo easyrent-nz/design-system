a33c86aaff579cc4099612b83dbd681aa207ba12
alexn400 <alexn400@gmail.com>
2022-02-06 16:09:11 +1300

    Improve file upload components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,10 +3,8 @@ export * from './icons';
 
 export { default as AspectImage } from './AspectImage/AspectImage';
 export { default as ReservedBanner } from './ReservedBanner/ReservedBanner';
-export { default as Hero } from './Hero/Hero';
 export { default as MapView } from './MapView/MapView';
 
 export type { AspectImageProps } from './AspectImage/AspectImage';
 export type { ReservedBannerProps } from './ReservedBanner/ReservedBanner';
-export type { HeroProps } from './Hero/Hero';
 export type { MapViewProps } from './MapView/MapView';
